871b032603483cb192b1f8e4d06e252993bc8297
Prachi Deodatta Gore <77486432+cigi10@users.noreply.github.com>
2025-07-27 17:14:05 +0530

    Update shell.h

--- kernel/shell/shell.h
+++ kernel/shell/shell.h
@@ -1,12 +1,8 @@
 #ifndef SHELL_H
 #define SHELL_H
 
-// initialize the shell system
 void shell_init(void);
-
-// main shell loop - runs interactive commands
 void shell_run(void);
-
 void console_gets_with_history(char* buffer, int max_len);
 
 #endif
