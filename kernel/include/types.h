6b1aac0d26015d3c9986ca55bdcbfe3e3501d393
Prachi Deodatta Gore <77486432+cigi10@users.noreply.github.com>
2025-07-27 17:29:00 +0530

    Update types.h

--- kernel/include/types.h
+++ kernel/include/types.h
@@ -1,26 +1,22 @@
 #ifndef TYPES_H
 #define TYPES_H
-
-// Block system stdint.h from being included
 #define _STDINT_H
 #define _STDINT_GCC_H
 
-// Boolean type
 typedef enum { bool_false = 0, bool_true = 1 } bool;
 #define false bool_false
 #define true bool_true
+  
+typedef long unsigned int uint64_t; 
+typedef long int int64_t;
+typedef unsigned int uint32_t;
+typedef int int32_t;
+typedef unsigned short uint16_t;
+typedef short int16_t;
+typedef unsigned char uint8_t;
+typedef signed char int8_t;
 
-// Exact types that match your RISC-V compiler
-typedef long unsigned int   uint64_t;  // Note: 'long' not 'long long'
-typedef long int            int64_t;
-typedef unsigned int        uint32_t;
-typedef int                 int32_t;
-typedef unsigned short      uint16_t;
-typedef short               int16_t;
-typedef unsigned char       uint8_t;
-typedef signed char         int8_t;
-
-// Architecture-specific types
+// architecture-specific types
 #if __riscv_xlen == 64
 typedef uint64_t size_t;
 typedef uint64_t uintptr_t;
@@ -35,4 +31,4 @@ typedef int32_t  intptr_t;
 #define NULL ((void*)0)
 #endif
 
-#endif /* TYPES_H */
+#endif 
