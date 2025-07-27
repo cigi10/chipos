1c2b5dc265dd6e47bbeeaffeb400cac934673c2b
Prachi Deodatta Gore <77486432+cigi10@users.noreply.github.com>
2025-07-27 16:50:08 +0530

    Update string.h

--- lib/string.h
+++ lib/string.h
@@ -1,14 +1,12 @@
 #ifndef STRING_H
 #define STRING_H
 
-#include <stddef.h> // For size_t
+#include <stddef.h> 
 
-/* Memory functions */
 void* memset(void* ptr, int value, size_t num);
 void* memcpy(void* dest, const void* src, size_t num);
 int memcmp(const void* ptr1, const void* ptr2, size_t num);
 
-/* String functions */
 size_t strlen(const char* str);
 int strcmp(const char* str1, const char* str2);
 int strncmp(const char* str1, const char* str2, size_t n);
@@ -21,12 +19,9 @@ char* strchr(const char* str, int c);
 char* strrchr(const char* str, int c);
 char* strtok(char* str, const char* delim);
 
-/* Additional useful functions */
 char* strtok(char* str, const char* delim);
 size_t strspn(const char* str1, const char* str2);
 size_t strcspn(const char* str1, const char* str2);
 
-
-
 char* itoa(int value, char* str, int base); 
-#endif // STRING_H
+#endif 
